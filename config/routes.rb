2ef53a0b25b2662e658fd9d64ed68431c763b77f
Adrian Barajas <a_barajas_c@hotmail.com>
2017-04-27 03:18:20 +0000

    Stock model and spinner functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "welcome#index"
+  root "users#my_portfolio"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "welcome/index", to: "welcome#index"
+  
+  get "my_portfolio", to: "users#my_portfolio"
+  get "search_stocks", to: "stocks#search"
 end
