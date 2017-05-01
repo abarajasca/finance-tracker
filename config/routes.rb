915caa42e7b71ab57c1916f51d146eb59c70412a
Adrian Barajas <a_barajas_c@hotmail.com>
2017-05-01 01:34:02 +0000

    Add userstock and first and last name and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
+  resources :user_stocks, except: [:show, :edit, :update]
+  
   root "users#my_portfolio"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
